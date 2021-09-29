a77aab119fefcc028312d11c5ca4b50e3b85db69
bhargaviRdy <46837526+bhargaviRdy@users.noreply.github.com>
2021-09-29 23:39:22 +0530

    update: done completely

---
+++ script.js
@@ -0,0 +1,32 @@
+var initialPrice = document.querySelector("#initialPrice");
+var quantity = document.querySelector("#quanitity");
+var currentPrice = document.querySelector("#currentPrice");
+var showProfit = document.querySelector("#showProfit");
+var result = document.querySelector("#result");
+var error = document.querySelector("#error");
+
+
+showProfit.addEventListener("click", function calculate() {
+
+    if (initialPrice.value.length === 0 || quantity.value.length === 0 || currentPrice.value.length === 0) {
+        error.innerText = "Enter all the values";
+        result.innerText = "";
+    }
+    else if (Number(initialPrice.value) > Number(currentPrice.value)) {
+        var loss = (Number(initialPrice.value) - Number(currentPrice.value)) * Number(quantity.value);
+        var lossPercentage = (loss / (Number(initialPrice.value)*Number(quantity.value))) * 100;
+        error.innerText = `Hey, the loss is ${loss} and the percent is ${lossPercentage}%`
+        result.innerText = "";
+    } 
+    else if (Number(currentPrice.value) > Number(initialPrice.value)) {
+        var profit = (Number(currentPrice.value) - Number(initialPrice.value)) * Number(quantity.value);
+        var profitPercentage = (profit / (Number(initialPrice.value)*Number(quantity.value))) * 100;
+        error.innerText = "";
+        result.innerText = `Hey, the profit is ${profit} and the percent is ${profitPercentage}%`
+    }
+     else {
+        error.innerText = "";
+        result.innerText = `No pain no gain and no gain no pain`;
+    }
+
+})
